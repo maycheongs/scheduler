2a0124a93c37586fa0a97ad1c30ce161b26dc28a
May C <maycheongs@gmail.com>
2021-02-16 00:01:41 -0500

    added more exmaple appointment data

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,27 +2,80 @@ import React, {useState} from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment"
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },  
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Michael Bubble",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }  
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Sarah Henley",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }  
+  },
+  {
+    id: "last",
+    time: "5pm",
+  },
+];
+
 
 export default function Application(props) {
 
   const [day,setDay] = useState("Monday")
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
 
   return (
     <main className="layout">
@@ -47,7 +100,14 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => {
+          return (
+            <Appointment
+            key={appointment.id}
+            {...appointment}
+            />
+          )
+        })}
         
       </section>
     </main>
