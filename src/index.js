610b18572e7d17008b38e49b8938597d3d8c3b58
May C <maycheongs@gmail.com>
2021-02-23 13:19:47 -0500

    updated netlify toml and axios baseurl in index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import axios from 'axios'
 import React from "react";
 import ReactDOM from "react-dom";
 
 import "index.scss";
 
 import Application from "components/Application";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 ReactDOM.render(<Application />, document.getElementById("root"));
